8ad0045306c640225aad6b17f5bf2a661df93103
elyutin <anatolyu128@gmail.com>
2023-02-02 00:26:16 +0500

    phpstan: installing & fixing

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,11 +1,11 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Back1ng\ChatGPTLaravelIgnition;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use OpenAI\Client;
 
-class ServiceProvider extends BaseServiceProvider
+final class ServiceProvider extends BaseServiceProvider
 {
     /**
      * Register any application services.
@@ -22,17 +22,5 @@ class ServiceProvider extends BaseServiceProvider
         $this->publishes([
             __DIR__.'/../config/back1ng-laravel-ignition.php' => config_path('back1ng-laravel-ignition.php'),
         ]);
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array<int, string>
-     */
-    public function provides(): array
-    {
-        return [
-            Client::class,
-        ];
     }
 }
--- src/Solutions/ChatGPTSolution.php
+++ src/Solutions/ChatGPTSolution.php
@@ -1,12 +1,14 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Back1ng\ChatGPTLaravelIgnition\Solutions;
 
+use InvalidArgumentException;
+use OpenAI;
 use Spatie\Ignition\Contracts\BaseSolution;
 use Spatie\Ignition\Contracts\HasSolutionsForThrowable;
 use Throwable;
 
-class ChatGPTSolution implements HasSolutionsForThrowable
+final class ChatGPTSolution implements HasSolutionsForThrowable
 {
     public function canSolve(Throwable $throwable): bool
     {
@@ -25,6 +27,10 @@ class ChatGPTSolution implements HasSolutionsForThrowable
     {
         $apiKey = config('back1ng-laravel-ignition.api_key');
 
+        if (! is_string($apiKey)) {
+            throw new InvalidArgumentException('Incorrect Api Key given.');
+        }
+
         $requestString = sprintf(
             'How can I fix this error in PHP? %s %s %s',
             get_class($throwable),
@@ -32,7 +38,7 @@ class ChatGPTSolution implements HasSolutionsForThrowable
             $throwable->getTraceAsString()
         );
 
-        $client = \OpenAI::client($apiKey);
+        $client = OpenAI::client($apiKey);
 
         $result = $client->completions()->create([
             'model' => 'text-davinci-003',
